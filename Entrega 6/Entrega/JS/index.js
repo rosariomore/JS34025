5fe3f56aa345a844cfd2a0314fe367dc85715e7c
rosariomore <rosariomore@hotmail.com>
2022-09-29 18:56:02 -0300

    nueva entrega eventos

--- Entrega 6/Entrega/JS/index.js
+++ Entrega 6/Entrega/JS/index.js
@@ -1,11 +1,13 @@
-
-//DOM
+/* DOM
 
 let contenedor = document.getElementById("container");
 let promociones = [
-    { id: 1, nombre: "Río de Janeiro", precio: 71.420 },    
+    { id: 1, nombre: "Río de Janeiro", precio: 71.419 },    
     { id: 2, nombre: "Salvador de Bahía", precio: 89.383 },
     { id: 3, nombre: "San Andrés", precio: 136.665 },
+    { id: 4, nombre: "Cancún", precio: 144.370 },
+    { id: 5, nombre: "Miami", precio: 162.394 },
+    { id: 6, nombre: "Punta Cana", precio: 160.427 },
   ];
 
   for (const promocion of promociones){
@@ -15,5 +17,4 @@ let promociones = [
             <p>Producto: ${promocion.nombre}</p>
             <b>$${promocion.precio}</b>`;
             contenedor.append(li);
-  }
-
+  };*/
---
+++ Entrega 6/Entrega/JS/servicios.js
@@ -0,0 +1,20 @@
+
+ // Evento uno
+
+const boton = document.getElementById("btn");
+
+boton.addEventListener("click", respuesta);
+
+function respuesta(){
+    console.log("Respuesta al click!");
+};
+
+/* Evento dos
+
+const botonDos = document.getElementById("formulario");
+
+botonDos.addEventListener("click", respuestaDos);
+
+function respuestaDos(){
+    console.log("Otra respuesta al click!");
+};*/
